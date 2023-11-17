9242e2b607be89a999fa2c1f4abe5349ca3e4ac4
iamgrg <gregoire.ansard@gmail.com>
2023-11-17 18:33:13 +0100

    sur un tous les ports

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: gregoire <gregoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 12:32:05 by gregoire          #+#    #+#             */
-/*   Updated: 2023/11/17 15:41:23 by gregoire         ###   ########.fr       */
+/*   Updated: 2023/11/17 18:26:13 by gregoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,40 +25,46 @@ Server::Server(Config const & config) : _config(config), _routes(config){
 Server::~Server() { }
 
 void Server::_initialize() {
-    // Créer un socket
-    if ((this->_listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        std::cerr << "Error: Could not create socket." << std::endl;
-        exit(EXIT_FAILURE);
+    std::vector<int>::const_iterator it;
+    for(it =  _config.getPorts().begin(); it != _config.getPorts().end(); ++it){
+        int port = *it;
+        int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (listen_fd == -1) {
+            std::cerr << "Error: Could not create socket." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        int opt = 1;
+        if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+            std::cerr << "Error: setsockopt failed." << std::endl;
+            close(listen_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        struct sockaddr_in server_addr;
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = inet_addr(_config.getHost().c_str());
+        server_addr.sin_port = htons(port);
+
+        if (bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+            std::cerr << "Error: bind failed." << std::endl;
+            close(listen_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(listen_fd, 10) == -1) {
+            std::cerr << "Error: listen failed." << std::endl;
+            close(listen_fd);
+            exit(EXIT_FAILURE);
+        }
+        _listen_fds.push_back(listen_fd);
     }
-    // Configurer les options du socket
-    int opt = 1;
-    if (setsockopt(_listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-        std::cerr << "Error: setsockopt failed." << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    // Préparer la structure sockaddr_in
-    memset(&_server_addr, 0, sizeof(_server_addr));
-    _server_addr.sin_family = AF_INET;
-    std::string ipString = this->_config.getHost();
-    _server_addr.sin_addr.s_addr = inet_addr(ipString.c_str());
-    _server_addr.sin_port = htons(this->_config.getPorts().at(0));
-    // _server_addr.sin_port = htons(this->_config.getPort());
-    // Lier le socket à l'adresse et au port
-    if (bind(_listen_fd, (struct sockaddr*)&_server_addr, sizeof(_server_addr)) == -1) {
-        std::cerr << "Error: bind failed." << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    // std::cout << "Server initialized" << std::endl;
 }
 
 
 void Server::start() {
     _isRunning = true;
-    // Écouter sur le socket
-    if (listen(this->_listen_fd, 10) == -1) {
-        std::cerr << "Error: listen failed." << std::endl;
-        exit(EXIT_FAILURE);
-    }
     std::cout << "Server started and listening on port " << this->_config.getPorts().at(0) << "..." << std::endl;
     while (_isRunning) {
         _handleRequests();
@@ -67,9 +73,44 @@ void Server::start() {
 
 void Server::stop() {
     _isRunning = false;
-    // Fermez les sockets ou faites d'autres opérations de nettoyage ici.
+    for (std::vector<int>::iterator it = _listen_fds.begin(); it != _listen_fds.end(); ++it) {
+        close(*it);
+    }
+    for (std::vector<int>::iterator it = _clients.begin(); it != _clients.end(); ++it) {
+        close(*it);
+    }
     std::cout << "Server stopped" << std::endl;
 }
+
+
+void Server::_handleRequests() {
+    int max_fd = 0;
+    std::set<int>::iterator fd;
+    _prepareSocketSet(max_fd);
+
+    fd_set tmpfds = _readfds;
+    if (select(max_fd + 1, &tmpfds, NULL, NULL, NULL) < 0) {
+        perror("select");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i <= max_fd; ++i) {
+        if (FD_ISSET(i, &tmpfds)) {
+            if (std::find(_listen_fds.begin(), _listen_fds.end(), i) != _listen_fds.end()) {
+                _acceptNewConnection(i);
+            } else {
+                _processClientRequest(i);
+            }
+        }
+    }
+    for (fd = _clientsToClose.begin(); fd != _clientsToClose.end(); ++fd) {
+        close(*fd);
+        FD_CLR(*fd, &_readfds);
+        _clients.erase(std::remove(_clients.begin(), _clients.end(), *fd), _clients.end());
+    }
+    _clientsToClose.clear();
+}
+
 void Server::_sendResponse(Response const &response, int const &client) {
     std::string responseString = response.buildResponse();
     if (send(client, responseString.c_str(), responseString.length(), 0) == -1) {
@@ -78,71 +119,79 @@ void Server::_sendResponse(Response const &response, int const &client) {
     }
 }
 
-void Server::_handleRequests() {
+void Server::_prepareSocketSet(int &max_fd) {
     FD_ZERO(&_readfds);
-    FD_SET(this->_listen_fd, &_readfds);
-    int max_fd = this->_listen_fd;
+    
+    // Ajouter les sockets d'écoute à _readfds
+    for (std::vector<int>::iterator it = _listen_fds.begin(); it != _listen_fds.end(); ++it) {
+        FD_SET(*it, &_readfds);
+        max_fd = std::max(max_fd, *it);
+    }
 
-    while (_isRunning) {
-        fd_set tmpfds = _readfds;
-        if (select(max_fd + 1, &tmpfds, NULL, NULL, NULL) < 0) {
-            perror("select");
-            exit(EXIT_FAILURE);
-        }
-        for (int i = 0; i <= max_fd; ++i) {
-            if (FD_ISSET(i, &tmpfds)) {
-                if (i == this->_listen_fd) {
-                    // Accepter la nouvelle connexion
-                    struct sockaddr_in client_addr;
-                    socklen_t addrlen = sizeof(client_addr);
-                    int client_fd = accept(this->_listen_fd, (struct sockaddr *)&client_addr, &addrlen);
-                    if (client_fd < 0) {
-                        perror("accept");
-                        continue;
-                    }
-                    FD_SET(client_fd, &_readfds);
-                    max_fd = std::max(max_fd, client_fd);
-                    std::cout << "Nouvelle connexion acceptée : " << client_fd << std::endl;
-                } else {
-                    char buffer[10000];
-                    int bytesRead = recv(i, buffer, sizeof(buffer), 0);
-                    if (bytesRead <= 0) {
-                        if (bytesRead < 0)
-                                throw std::runtime_error("Recv failed");
-                        close(i);
-                        FD_CLR(i, &_readfds);
-                        _clients.erase(std::remove(_clients.begin(), _clients.end(), i), _clients.end());
-                    } else {
-                        std::string message(buffer, bytesRead);
-                        // A FAIRE !! : check validité du format du message => pas de requetes HTTPS ou autres formats
-                        Request request(message, this->_config.getMaxBodySize());
-                        // && request.getHeader("Host") + ":" + std::to_string(_config.getPorts().at(1)) != this->_config.getHost())
-                        bool validHost = false;
-                        for (std::vector<std::string>::const_iterator it = this->_config.getNames().begin(); it != this->_config.getNames().end(); ++it)
-                        {
-                            if(request.getHeader("Host") == *it + ":" + std::to_string(this->_config.getPorts().at(0)))
-                                validHost = true;
-                        }
-                        if(!validHost)
-                        {
-                            std::string error = "HTTP/1.1 400 Bad Request\r\n\r\n";
-                            if (send(i, error.c_str(), error.length(), 0) == -1) {
-                                std::cerr << "Error: send failed." << std::endl;
-                                exit(EXIT_FAILURE);
-                            }
-                            close(i);
-                            FD_CLR(i, &_readfds);
-                            continue;
-                        }
-                        std::cout << request << std::endl;
-                        Response response = _routes.handle(request);
-                        this->_sendResponse(response, i);
-                    }
-                    close(i);
-                    FD_CLR(i, &_readfds);
-                }
-            }
+    // Ajouter les sockets clients valides à _readfds
+    std::vector<int>::iterator it = _clients.begin();
+    while (it != _clients.end()) {
+        if (*it >= 0) {  // Vérifiez que le descripteur est valide
+            FD_SET(*it, &_readfds);
+            max_fd = std::max(max_fd, *it);
+            ++it;
+        } else {
+            it = _clients.erase(it);  // Supprimez les descripteurs invalides
         }
     }
 }
 
+
+void Server::_acceptNewConnection(int listen_fd) {
+    struct sockaddr_in client_addr;
+    socklen_t addrlen = sizeof(client_addr);
+    int client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &addrlen);
+    if (client_fd < 0) {
+        perror("accept");
+        return;
+    }
+    FD_SET(client_fd, &_readfds);
+    _clients.push_back(client_fd);
+    std::cout << "Nouvelle connexion acceptée : " << client_fd << std::endl;
+}
+
+void Server::_processClientRequest(int client_fd) {
+    char buffer[10000];
+    int bytesRead = recv(client_fd, buffer, sizeof(buffer), 0);
+    if (bytesRead <= 0) {
+        if (bytesRead < 0) {
+            std::cerr << "Recv failed" << std::endl;
+        }
+        close(client_fd);
+        FD_CLR(client_fd, &_readfds);
+        _clients.erase(std::remove(_clients.begin(), _clients.end(), client_fd), _clients.end());
+        _clientsToClose.insert(client_fd);
+        return;
+    }
+    std::string message(buffer, bytesRead);
+    // A FAIRE !! : check validité du format du message => pas de requetes HTTPS ou autres formats
+    Request request(message, this->_config.getMaxBodySize());
+    bool validHost = false;
+    for (std::vector<std::string>::const_iterator it = this->_config.getNames().begin(); it != this->_config.getNames().end(); ++it)
+    {
+        if(request.getHeader("Host") == *it + ":" + std::to_string(this->_config.getPorts().at(0))
+            || request.getHeader("Host") == *it + ":" + std::to_string(this->_config.getPorts().at(1)))
+            validHost = true;
+    }
+    std::cout << "PORT[1] : " << this->_config.getPorts().at(1) << std::endl;
+    if(!validHost)
+    {
+        std::string error = "HTTP/1.1 400 Bad Request\r\n\r\n";
+        if (send(client_fd, error.c_str(), error.length(), 0) == -1) {
+            std::cerr << "Error: send failed." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        close(client_fd);
+        FD_CLR(client_fd, &_readfds);
+        return;
+    }
+    std::cout << request << std::endl;
+    Response response = _routes.handle(request);
+    this->_sendResponse(response, client_fd);
+    _clientsToClose.insert(client_fd);
+}
--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: gregoire <gregoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 12:32:01 by gregoire          #+#    #+#             */
-/*   Updated: 2023/11/11 16:47:29 by gregoire         ###   ########.fr       */
+/*   Updated: 2023/11/17 18:18:17 by gregoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sstream>
+#include <set>
 # include "../config/Config.hpp"
 # include "../request/Request.hpp"
 # include "../response/Response.hpp"
@@ -38,15 +39,21 @@ class Server {
     private:
         Config _config;
         Routes _routes;
-        int _listen_fd;
+        // int _listen_fd;
+        std::vector<int> _listen_fds;
         bool _isRunning;
-        struct sockaddr_in _server_addr;
+        // struct sockaddr_in _server_addr;
         fd_set _readfds;
         std::vector<int> _clients;
+        std::set<int> _clientsToClose;
         
         void _sendResponse(Response const & response, int const &client);
         void _initialize();
+        // void _manageConnections();
+        void _processClientRequest(int client_fd);
+        void _acceptNewConnection(int listen_fd);
         void _handleRequests();
+        void _prepareSocketSet(int &max_fd);
 };
 
 #endif // SERVER_HPP
